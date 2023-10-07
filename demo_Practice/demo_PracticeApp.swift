a5c91285f1d5746e0cdad8b2fa3a5e9a44b83345
Nayan Dave <61861453+imnayandave@users.noreply.github.com>
2023-10-07 17:05:12 +0530

    Initial Commit

---
+++ demo_Practice/demo_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_PracticeApp.swift
+//  demo_Practice
+//
+//  Created by Nayan Dave on 07/10/23.
+//
+
+import SwiftUI
+
+@main
+struct demo_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
